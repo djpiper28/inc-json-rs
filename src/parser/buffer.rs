ca64edce6fd2637383144771cf98c58a69160835
Danny Piper <djpiper28@gmail.com>
2024-09-27 22:55:40 +0100

    feat: added more data flag

--- src/parser/buffer.rs
+++ src/parser/buffer.rs
@@ -4,6 +4,8 @@ use std::string::String;
 struct Buffer {
     buffer: String,
     current_path: JsonPath,
+    /// Whether or not there is more data to be expected after the end of the buffer.
+    more_data: bool,
 }
 
 impl Buffer {
@@ -12,6 +14,7 @@ impl Buffer {
         Buffer {
             buffer: String::new(),
             current_path: Vec::new(),
+            more_data: true,
         }
     }
 }
