450d3cb8eef34988b52a28c604a3e47a87138dad
Danny Piper <djpiper28@gmail.com>
2024-09-29 21:10:20 +0100

    faet: wrote some tests

--- src/primitives/null.rs
+++ src/primitives/null.rs
@@ -1,8 +1,18 @@
-const NULL: &str = "null";
+pub const NULL: &str = "null";
 
 pub fn is_first_char_of_null(c: char) -> bool {
     match (c) {
         'n' => true,
         _ => false,
+    }
+}
+
+#[cfg(test)]
+mod test_null_primitive {
+    use super::*;
+
+    #[test]
+    fn test_null_is_first_char_of_null_is_true() {
+        assert!(is_first_char_of_null(NULL.chars().next().unwrap()));
     }
 }
--- src/primitives/number.rs
+++ src/primitives/number.rs
@@ -6,3 +6,27 @@ pub fn is_first_char_of_number(c: char) -> bool {
         _ => false,
     }
 }
+
+#[cfg(test)]
+mod test_number_primitive {
+    use super::*;
+
+    #[test]
+    fn test_is_first_char_of_number_digit() {
+        for i in 0..9 {
+            assert!(is_first_char_of_number(
+                i.to_string().chars().next().unwrap()
+            ));
+        }
+    }
+
+    #[test]
+    fn test_is_first_char_of_number_plus() {
+        assert!(is_first_char_of_number('+'))
+    }
+
+    #[test]
+    fn test_is_first_char_of_number_minus() {
+        assert!(is_first_char_of_number('-'))
+    }
+}
