aa903add1240257ca1d0847fe0f02b54c3892ef3
Danny Piper <djpiper28@gmail.com>
2024-10-20 19:36:01 +0100

    feat: start tokenisation

--- src/parser/tokens/mod.rs
+++ src/parser/tokens/mod.rs
@@ -1,1 +1,14 @@
+use number::Number;
+
+pub mod number;
 pub mod whitespace;
+
+pub enum Tokens {
+    Whitespace,
+    Null,
+    Boolean(bool),
+    Number(Number),
+    // String(String),
+    // Object(Object),
+    // Array(Array),
+}
---
+++ src/parser/tokens/number.rs
@@ -0,0 +1,4 @@
+pub enum Number {
+    Integer(i64),
+    Float(f64),
+}
