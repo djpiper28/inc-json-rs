38e31792c2525c996e72843ac0aee44f5380b115
Danny Piper <djpiper28@gmail.com>
2024-10-20 17:45:26 +0100

    feat: expose buffer

--- src/parser/buffer.rs
+++ src/parser/buffer.rs
@@ -1,7 +1,7 @@
 use super::json_path::JsonPath;
 use std::string::String;
 
-struct Buffer {
+pub struct Buffer {
     buffer: String,
     current_path: JsonPath,
     /// Whether or not there is more data to be expected after the end of the buffer.
