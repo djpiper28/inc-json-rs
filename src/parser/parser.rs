371cd4e65bfd13c065ed69a97a3bfdc77c6e4c9f
Danny Piper <danny.piper@motorolasolutions.com>
2024-12-13 16:26:18 +0000

    feat: added scanner output handler

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -6,6 +6,13 @@ use super::{
     lexer::{scanners::scan_token, tokens::JsonToken},
 };
 
+enum CurrentlyScanning {
+    Array,
+    Object,
+    /// The key is the value
+    KeyValuePair(String),
+}
+
 pub struct Parser {
     json_path: ObjectConsumer,
 }
@@ -16,34 +23,38 @@ impl Parser {
     }
 
     pub async fn parse(self, buffer: &mut Pin<Box<&mut Buffer>>) -> Result<(), &'static str> {
+        let mut state_stack: Vec<CurrentlyScanning> = Vec::new();
+
         loop {
             if buffer.is_eof().await {
                 break;
             }
 
-            let res = match buffer.next_char().await {
+            let res: Result<JsonToken, &'static str> = match buffer.next_char().await {
                 Ok(c) => match scan_token(c, buffer).await {
-                    Ok(token) => match token {
-                        JsonToken::Whitespace => todo!("handle me"),
-                        JsonToken::Null => todo!("handle me"),
-                        JsonToken::Boolean(boolean) => todo!("handle me"),
-                        JsonToken::Number(number) => todo!("handle me"),
-                        JsonToken::String(string) => todo!("handle me"),
-                        JsonToken::ObjectStart => todo!("handle me"),
-                        JsonToken::ObjectEnd => todo!("handle me"),
-                        JsonToken::ObjectValueIndicator => todo!("handle me"),
-                        JsonToken::ArrayStart => todo!("handle me"),
-                        JsonToken::ArrayEnd => todo!("handle me"),
-                        JsonToken::Comma => todo!("handle me"),
-                    },
+                    Ok(token) => Ok(token),
                     Err(e) => Err(e),
                 },
                 Err(e) => Err(e),
             };
 
             if res.is_err() {
-                return res;
+                return Err(res.err().unwrap());
             }
+
+            let parse_res: Result<(), &'static str> = match res.unwrap() {
+                JsonToken::Whitespace => Ok(()),
+                JsonToken::Null => todo!("handle me"),
+                JsonToken::Boolean(boolean) => todo!("handle me"),
+                JsonToken::Number(number) => todo!("handle me"),
+                JsonToken::String(string) => todo!("handle me"),
+                JsonToken::ObjectValueIndicator => todo!("handle me"),
+                JsonToken::Comma => todo!("handle me"),
+                JsonToken::ArrayStart => todo!("handle me"),
+                JsonToken::ObjectStart => todo!("handle me"),
+                JsonToken::ArrayEnd => todo!("handle me"),
+                JsonToken::ObjectEnd => todo!("handle me"),
+            };
         }
 
         return Ok(());
